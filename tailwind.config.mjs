cecd0a9236c2921b9ac146667089b285b2aa0c82
jackberck <berckjack@gmail.com>
2024-10-22 09:22:43 +0700

    feat: adding navbar and footer section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        nunito: "Nunito",
+      },
+      colors: {
+        "dark-base": "#020617",
+        "light-base": "#f1f5f9",
+        "blue-base": "#1d4ed8",
+        "green-base": "#16a34a",
+      },
+    },
+  },
+  plugins: [],
+};
