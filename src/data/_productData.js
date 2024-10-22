cec30c1ecf161bac125995000462643aa001661b
jackberck <berckjack@gmail.com>
2024-10-22 23:58:21 +0700

    feat: adding profile page

---
+++ src/data/_productData.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    image: "/images/products/grand-astrea-96.jpg",
+    title: "Honda Grand Astrea 96",
+    price: 5000000,
+    description:
+      "Motor Honda Grand Astrea 96 dengan kondisi bagus, mesin halus, dan siap pakai.",
+    condition: "Bekas",
+    category: "Motor",
+  },
+  {
+    image: "/images/products/honda-goldwing.jpg",
+    title: "Honda Goldwing",
+    price: 100000000,
+    description:
+      "Motor Honda Goldwing dengan kondisi bagus layak pakai dan siap touring.",
+    condition: "Bekas",
+    category: "Motor",
+  },
+  {
+    image: "/images/products/lenovo-yoga-pro-9.jpg",
+    title: "Lenovo Yoga Pro 9",
+    price: 15000000,
+    description:
+      "Laptop Lenovo Yoga Pro 9 dengan spesifikasi tinggi, layar touchscreen, dan ringan.",
+    condition: "Bekas",
+    category: "Laptop",
+  },
+  {
+    image: "/images/products/rog-phone-2.jpg",
+    title: "Asus ROG Phone 2",
+    price: 8000000,
+    description:
+      "Handphone Asus ROG Phone 2 dengan spesifikasi gaming, layar 120Hz, dan performa tinggi.",
+    condition: "Bekas",
+    category: "Handphone",
+  },
+  {
+    image: "/images/products/sony-a7-mark-2.jpg",
+    title: "Sony A7 Mark 2",
+    price: 12000000,
+    description:
+      "Kamera Sony A7 Mark 2 dengan kualitas gambar tinggi, full frame, dan performa tinggi.",
+    condition: "Bekas",
+    category: "Kamera",
+  },
+  {
+    image: "/images/products/tuf-gaming.jpg",
+    title: "TUF Gaming",
+    price: 20000000,
+    description:
+      "Laptop TUF Gaming dengan spesifikasi tinggi, performa tinggi, dan tahan lama.",
+    condition: "Bekas",
+    category: "Laptop",
+  },
+];
+
+export default products;
